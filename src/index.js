c7ab03ef27fc182da1739d3368af819922c4c377
Carlos Daniel <carlos.daniel@devsquad.email>
2021-03-18 21:23:46 -0300

    creating folder for ex-2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-import routes from './routes';
+import routes from './ex-2/routes';
 
 const app = express();
 
