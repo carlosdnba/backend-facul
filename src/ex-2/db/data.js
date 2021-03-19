a37250f952365b73031d466f8eaa122eba0580a4
Carlos Daniel <carlos.daniel@devsquad.email>
2021-03-18 21:32:59 -0300

    using data file instead of connection with database

---
+++ src/ex-2/db/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    nome: 'Inspiron',
+    marca: 'Dell',
+    tipo: 'Eletrônico',
+  },
+  {
+    nome: 'S10',
+    marca: 'Samsung',
+    tipo: 'Eletrônico',
+  },
+  {
+    nome: 'Caderno',
+    marca: 'Tilibra',
+    tipo: 'Escolar',
+  },
+  {
+    nome: 'Caneta azul 7mm',
+    marca: 'BIC',
+    tipo: 'Escolar',
+  },
+];
